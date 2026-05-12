f0b8f9993d40ba20a3bc03045b519e64e305a548
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2026-05-13 00:52:47 +0100

    update

--- hotel-housekeeping-service/src/main/java/com/hotel/housekeeping/HousekeepingServiceApplication.java
+++ hotel-housekeeping-service/src/main/java/com/hotel/housekeeping/HousekeepingServiceApplication.java
@@ -1,9 +1,11 @@
 package com.hotel.housekeeping;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.hotel.housekeeping", "com.hotel.common"})
+@EnableRabbit
 public class HousekeepingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(HousekeepingServiceApplication.class, args);
