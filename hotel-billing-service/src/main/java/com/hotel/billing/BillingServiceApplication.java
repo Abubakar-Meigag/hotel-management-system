76ee176e46edb1ee63a0938b709dcd381b370ad6
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2026-05-13 01:22:43 +0100

    fix billing

--- hotel-billing-service/src/main/java/com/hotel/billing/BillingServiceApplication.java
+++ hotel-billing-service/src/main/java/com/hotel/billing/BillingServiceApplication.java
@@ -1,9 +1,11 @@
 package com.hotel.billing;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.hotel.billing", "com.hotel.common"})
+@EnableRabbit
 public class BillingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BillingServiceApplication.class, args);
